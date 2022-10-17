7f586ca83bb58a79ea3f147d1e10dd26542965f6
Matthew Cwalina <matthewcwalina@gmail.com>
2022-10-17 16:24:25 +0200

    Added cookie to post of start time to track the time

--- challenge/public/api/index.php
+++ challenge/public/api/index.php
@@ -23,14 +23,22 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $app->post('/starttime', function (Request $request, Response $response) {
     //Retrieving that body from the post URL
     $data = $request->getParsedBody();
+    $results = $data['start_time'];
 
     //Using redis to cache the $data (simulating inserting data into DB)
     $redisClient = $this->get('redisClient');
     $redisClient->set('start_time', json_encode($data), 'EX', 100);
 
-
+    //Create a cookie to track the start time
+    $cookieValue = '';
+    if (empty($_COOKIE["Expiration"])) {
+        $cookieName = "Expiration";
+        $cookieValue = (string)time();
+        $expires = time() + 60 * 60 * 24 * 30; // 30 days.
+        setcookie($cookieName, $cookieValue, $expires, '/');
+    }
     //Taking the response and producing JSON
-    $response->getBody()->write(json_encode($data, JSON_THROW_ON_ERROR));
+    $response->getBody()->write(json_encode(["start_time" => $results, 'expiration_time' => $_COOKIE["Expiration"] ?? $cookieValue,], JSON_THROW_ON_ERROR));
 
     return $response->withHeader('Content-Type', 'application/json');
 
@@ -41,14 +49,6 @@ $app->get('/starttime', function (Request $request, Response $response) {
     //Retrieving data from our redis cache
     $redisClient = $this->get('redisClient');
     $results = $redisClient->get('start_time');
-
-    $cookieValue = '';
-    if (empty($_COOKIE["expiration"])) {
-        $cookieName = "Expiration";
-        $cookieValue = (string)time();
-        $expires = time() + 60 * 60 * 24 * 30; // 30 days.
-        setcookie($cookieName, $cookieValue, $expires, '/');
-    }
 
     //Need this to turn JSON into string so I can encode the JSON again in the write() function
     $start_time = (json_decode($results));
